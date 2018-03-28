51eb18e24db9675fab7e28ae495fc56d2491578f
Edward L. Ries <eries@hightechhangout.com>
2018-03-28 10:39:36 -0400

    updated character

--- EverCraft.Model/Character.cs
+++ EverCraft.Model/Character.cs
@@ -8,6 +8,8 @@
     {
         public string Name { get; set; }
         public CharacterAlignment Alignment { get; set; }
+        public int ArmorClass { get; set; } = 10;
+        public int HitPoints { get; set; } = 5;
 
     }
 }
