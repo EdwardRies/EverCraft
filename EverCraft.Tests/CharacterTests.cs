c18ab4f0d403aea022f13d828a6845bd3ea2fb09
Edward L. Ries <eries@hightechhangout.com>
2018-03-28 10:56:06 -0400

    test
    test

--- EverCraft.Tests/CharacterTests.cs
+++ EverCraft.Tests/CharacterTests.cs
@@ -23,7 +23,7 @@ namespace EverCraft.Tests
 
 
         [TestMethod]
-        public void CharacterIsGood()
+        public void CharacterAlignmentIsGood()
         {
             var character = new Character();            
 
@@ -34,5 +34,52 @@ namespace EverCraft.Tests
             Assert.AreEqual(CharacterAlignment.Good, character.Alignment);
         }
 
+        [TestMethod]
+        public void CharacterHasDefaultArmor()
+        {
+            var character = new Character();
+
+            // Act
+            // No Act Necessary (Testing default value)
+
+            // Assert
+            Assert.AreEqual(10, character.ArmorClass);
+        }
+
+        [TestMethod]
+        public void CharacterHasUpgradedArmor()
+        {
+            var character = new Character();
+
+            // Act
+            character.ArmorClass = 20;
+
+            // Assert
+            Assert.AreEqual(20, character.ArmorClass);
+        }
+
+        [TestMethod]
+        public void CharacterHasDefaultHitPoints()
+        {
+            var character = new Character();
+
+            // Act
+            // No Act Necessary (Testing default value)
+
+            // Assert
+            Assert.AreEqual(5, character.HitPoints);
+        }
+
+        [TestMethod]
+        public void CharacterHasUpgradedHitPoints()
+        {
+            var character = new Character();
+
+            // Act
+            character.HitPoints = 10;
+
+            // Assert
+            Assert.AreEqual(10, character.HitPoints);
+        }
     }
 }
